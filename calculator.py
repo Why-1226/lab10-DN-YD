f8583a9adb879b07e1dff995d3adcb4f0f476056
Why.-.-. <Chuchinavas23@gmail.com>
2025-04-10 15:47:13 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -7,50 +7,22 @@ One function per operation, in order.
 # First example
 import math
 def add(a, b): 
-    a + b
-
-def sub(a, b):
-    a - b
-
-def mul(a, b):
-    a * b
-
-def div(a, b):
-    try:
-        b / a
-    except ZeroDivisionError:
-        print("Can't divide by zero")
-
-def log(a ,b):
-    try:
-        math.log(b, a)
-    except ValueError:
-        print("Must put in valid inputs")
-
-def exp(a, b):
-    a ** b
-
-# YD Section
-
-def square_root(a):
-    return math.sqrt(a)
-
-
-def hypotenuse(a,b):
-    return math.hypot(a,b)
-
-
-def add(a,b):
     return a + b
 
-def subtract(a,b):
+def sub(a, b):
     return a - b
 
-def multiply(a,b):
+def mul(a, b):
     return a * b
 
-def logarithm(a, b): # use math library/raise Value Error
-    return math.log(b,a)
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
 
-def exponent(a, b):
+def log(a ,b):
+    return math.log(b, a)
+
+def exp(a, b):
     return a ** b

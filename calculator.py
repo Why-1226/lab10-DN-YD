aa25bb22d21e80745a16503db2e21428c8fa23a4
Why.-.-. <Chuchinavas23@gmail.com>
2025-04-10 15:30:35 -0400

    Test commit

--- calculator.py
+++ calculator.py
@@ -7,28 +7,25 @@ One function per operation, in order.
 # First example
 import math
 def add(a, b): 
-    a + b
+    return a + b
 
 def sub(a, b):
-    a - b
+    return a - b
 
 def mul(a, b):
-    a * b
+    return a * b
 
 def div(a, b):
-    try:
-        b / a
-    except ZeroDivisionError:
-        print("Can't divide by zero")
+    if a == 0:
+        raise ZeroDivisionError
+    else:
+        return b / a
 
-def log(a ,b):
-    try:
-        math.log(b, a)
-    except ValueError:
-        print("Must put in valid inputs")
+def log(a, b):
+    return math.log(b, a)
 
 def exp(a, b):
-    a ** b
+    return a ** b
 
 
 
